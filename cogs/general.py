77b80ee1255470ccf847b17c2e13707fed4be931
GoldJust <117308582+NX-GoldJust@users.noreply.github.com>
2023-03-19 12:26:47 +0100

    Update general.py
    Ajout say

--- cogs/general.py
+++ cogs/general.py
@@ -17,7 +17,7 @@ class General(commands.Cog):
         
     @app_commands.command(name="help", description="Voir mes commandes")
     async def help(self, i: discord.Interaction):
-        embed = discord.Embed(title="Help", timestamp=datetime.now(), description="Voici mes commandes avec comme préfix `/`: \n\n**Générale :**\n`ping` - Avoir mon ping\n`help` - Avoir ce menu\n`info` - Information sur le bot\n\n**Modération :**\n`clear` - Supprimer des messages\n`kick` - Kick un membre\n`ban` - Bannir un membre\n`unban` - Débannir un membre\n`off` - Mettre off le bot")
+        embed = discord.Embed(title="Help", timestamp=datetime.now(), description="Voici mes commandes avec comme préfix `/`: \n\n**Générale :**\n`ping` - Avoir mon ping\n`help` - Avoir ce menu\n`info` - Information sur le bot\n`say` - Envoyer un message avec le bot(admin)\n\n**Modération :**\n`clear` - Supprimer des messages\n`kick` - Kick un membre\n`ban` - Bannir un membre\n`unban` - Débannir un membre\n`off` - Mettre off le bot")
         await i.response.send_message(embed=embed)
     
     @app_commands.command(name="info", description="Informations sur le bot")
@@ -27,12 +27,17 @@ class General(commands.Cog):
         embed.set_thumbnail(url=self.bot.user.avatar.url)
         await i.response.send_message(embed=embed)
         
-        
-        
-        
-        
-        
-        
+    @app_commands.command(name="say", description="Envoyer un message avec l'identité du bot")
+    @app_commands.describe(message="Le message")
+    @app_commands.describe(salon="Le salon")
+    @app_commands.default_permissions(administrator=True)
+    async def say(self, i: discord.Interaction, message: str, salon: discord.TextChannel=None):
+        if not salon:
+            salon = i.channel
+        embed = discord.Embed(title=None, description=message, timestamp=datetime.now())
+        embed.set_footer(text=i.user.name, icon_url=i.user.avatar.url)
+        await salon.send(embed=embed)
+        await i.response.send_message("Message envoyé", ephemeral=True)
         
         
 async def setup(bot):
